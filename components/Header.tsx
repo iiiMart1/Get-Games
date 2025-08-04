f6a43381bf0e4ae64d201a0e9078b403a2b98b31
MarwaneMohammedi <work.mohammedimarwane@gmail.com>
2025-08-04 08:20:53 +0100

    Fix: Logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,6 +9,7 @@ export function Header() {
           <div className="flex items-center gap-2">
             <img
               src="..\assets\Logo.svg"
+              alt = "Logo"
               className="w-8 h-8 object-contain rounded"
              />
             <h1 className="text-white text-lg bg-gradient-to-r from-cyan-400 to-purple-400 bg-clip-text text-transparent">
